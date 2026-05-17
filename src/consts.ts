ab6ac567a363e39301969c5b04165fc526931395
Shoe2 <andrewelee1983@gmail.com>
2026-05-16 18:18:50 -0700

    changes

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Quality Band Repair";
+export const SITE_DESCRIPTION = "We repair, rent, buy and sell musical instruments and equipment of all types, genres and cultures on the Seattle Eastside!";
